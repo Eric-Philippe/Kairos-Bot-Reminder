f7c35b51429f84d2f8e3d9912c036cea5d59802b
Eric-Philippe <ericphlpp@gmail.com>
2023-02-05 11:58:24 +0100

    Shell Script Clean

--- build.sh
+++ build.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
+echo "Pulling latest changes from git repository..."
+
 # Pull the latest changes from the git repository
 git pull
+
+echo "Installing dependencies..."
 
 # Build the project and copy the .env file from the root folder to the dist folder
 npm run build
 cp .env dist/.env
 
+echo "Restarting pm2 process..."
+
 # Restart the pm2 process
 pm2 restart Bot
+
+echo "Done!"
+
+# Path: .env
+#@Eric-Philippe
