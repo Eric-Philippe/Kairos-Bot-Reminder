30314bb427a93db829aae01a0111412a870f65ca
Eric-Philippe <ericphlpp@gmail.com>
2024-02-11 12:41:07 +0100

    Add Snooze Feature

--- src/Listener/fire.remindme.queue.ts
+++ src/Listener/fire.remindme.queue.ts
@@ -1,4 +1,17 @@
-import { Client, EmbedBuilder, TextChannel, User } from "discord.js";
+import {
+  ActionRow,
+  ActionRowBuilder,
+  ActionRowData,
+  ButtonBuilder,
+  ButtonInteraction,
+  ButtonStyle,
+  Client,
+  ComponentType,
+  EmbedBuilder,
+  MessageActionRowComponentBuilder,
+  TextChannel,
+  User,
+} from "discord.js";
 
 import Logger from "../logs/Logger";
 import { LogType } from "../logs/type.enum";
@@ -84,12 +97,11 @@ export default class FireRemindmeQueue implements FireQueue {
       res();
     });
   }
-  public sendMsg(
+  public async sendMsg(
     target: TextChannel | User,
     reminder: Remindme
   ): Promise<number> {
     return new Promise(async (res, rej) => {
-      // Log the instance of the target
       if (target instanceof User) {
         try {
           let attachment = await RemindmeDisplay(reminder);
@@ -97,12 +109,128 @@ export default class FireRemindmeQueue implements FireQueue {
             .setTitle("⌛ | You have a new reminder ! ⌛")
             .setColor("#5865F2");
 
+          // Create a button for snoozing the reminder
+          const snoozeButton = new ButtonBuilder()
+            .setCustomId("snooze")
+            .setLabel("Snooze")
+            .setStyle(ButtonStyle.Primary);
+
+          const row =
+            new ActionRowBuilder() as ActionRowBuilder<MessageActionRowComponentBuilder>;
+          row.addComponents(snoozeButton);
+
           await target.send({ embeds: [embed] });
+
           // Wait 2 seconds before sending the attachment
           await new Promise((res) => setTimeout(res, 2000));
-          await target.send({ files: [attachment] });
+          const msg = await target.send({
+            files: [attachment],
+            components: [row],
+          });
 
           res(0);
+
+          /**
+           * Snooze button
+           * We will listen for the button click
+           * If nothing is done for 30 minutes, the snooze button will be removed
+           * If it's clicked, we will show a list of snooze options (10 minutes, 1h, 8h, TOMORROW)
+           * If the user clicks on one of the options, we will recreate the reminder with the new date
+           */
+          try {
+            const snoozeResult = await msg.awaitMessageComponent({
+              time: 1800000,
+              componentType: ComponentType.Button,
+            });
+
+            if (snoozeResult.isButton()) {
+              const buttonInteraction = snoozeResult as ButtonInteraction;
+              if (buttonInteraction.customId === "snooze") {
+                // Snooze button clicked
+                await buttonInteraction.deferUpdate();
+                await buttonInteraction.editReply({
+                  content: "Snooze options",
+                  components: [],
+                });
+
+                const snoozeOptions = [
+                  {
+                    label: "10 minutes",
+                    value: 10,
+                  },
+                  {
+                    label: "1 hour",
+                    value: 60,
+                  },
+                  {
+                    label: "8 hours",
+                    value: 480,
+                  },
+                  {
+                    label: "Tomorrow",
+                    value: 1440,
+                  },
+                ];
+
+                const snoozeRow =
+                  new ActionRowBuilder() as ActionRowBuilder<ButtonBuilder>;
+                for (const option of snoozeOptions) {
+                  const button = new ButtonBuilder()
+                    .setCustomId(`snooze_${option.value}`)
+                    .setLabel(option.label)
+                    .setStyle(ButtonStyle.Primary);
+                  snoozeRow.addComponents(button);
+                }
+
+                await buttonInteraction.editReply({
+                  components: [snoozeRow],
+                });
+
+                const snoozeResult = await msg.awaitMessageComponent({
+                  time: 60000,
+                  componentType: ComponentType.Button,
+                });
+
+                if (snoozeResult.isButton()) {
+                  const buttonInteraction = snoozeResult as ButtonInteraction;
+                  const value = parseInt(
+                    buttonInteraction.customId.split("_")[1]
+                  );
+                  if (value) {
+                    const nextDate = new Date(
+                      reminder.targetDate.getTime() + value * 60000
+                    );
+                    await RemindmeServices.addRemindMe(
+                      reminder.content,
+                      reminder.description,
+                      reminder.entryDate,
+                      nextDate,
+                      reminder.repetition,
+                      0,
+                      reminder.RCId,
+                      reminder.userId
+                    );
+
+                    const snoozeEmbed = new EmbedBuilder()
+                      .setTitle("⌛ | Reminder snoozed ! ⌛")
+                      .setColor("#5865F2")
+                      .setDescription(`Snoozed for ${value} minutes`);
+
+                    await buttonInteraction.reply({
+                      content: `Snoozed for ${value} minutes`,
+                      components: [],
+                    });
+
+                    await msg.edit({
+                      components: [],
+                    });
+                  }
+                }
+              }
+            }
+          } catch (error) {
+            console.log(error);
+          }
         } catch (error) {
           rej(error);
         }
