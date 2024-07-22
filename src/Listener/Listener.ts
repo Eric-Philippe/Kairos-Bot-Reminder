c75de3b4c047433f8f54dbd63b7690dd317df5df
Eric-Philippe <ericphlpp@gmail.com>
2024-07-22 10:48:01 +0200

    Add Cron and Add CI/CD

--- src/Listener/Listener.ts
+++ src/Listener/Listener.ts
@@ -1,4 +1,7 @@
 import { Client } from "discord.js";
+
+var cron = require("node-cron");
+
 import FireRemindmeQueue from "./fire.remindme.queue";
 import FireRemindusQueue from "./fire.remindus.queue";
 // import Logger from "../logs/Logger";
@@ -6,7 +9,8 @@ import FireRemindusQueue from "./fire.remindus.queue";
 require("dotenv").config();
 
 const fireListener = async (client: Client) => {
-  setInterval(async () => {
+  // Run every minute
+  cron.schedule("*/1 * * * *", async () => {
     try {
       const remindmeQueue = new FireRemindmeQueue();
       remindmeQueue.fire(client);
@@ -20,7 +24,7 @@ const fireListener = async (client: Client) => {
     } catch (error) {
       console.log(error);
     }
-  }, 1000 * 60); // 1 minute;
+  });
 };
 
 export default fireListener;
