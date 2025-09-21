558a9038be1b822f58429d4952e0af38a7e93615
Eric-Philippe <ericphlpp@gmail.com>
2025-09-21 20:12:01 +0200

    Test

--- src/Commands.ts
+++ src/Commands.ts
@@ -1,17 +1,30 @@
-import { readdirSync } from "fs";
+import { readdirSync, existsSync } from "fs";
 import * as path from "path";
 
 import { Command } from "./CommandTemplate";
 
 const Commands: Command[] = [];
-// Loop through all the commands folder with a .js extension only
-readdirSync(path.join(__dirname, "commands"))
-  .filter((file) => file.endsWith(".js"))
-  .forEach((file) => {
-    // Get the command from the file
-    const command = require(path.join(__dirname, "commands", file)).default;
-    // Add the command to the commands array
-    Commands.push(command);
-  });
+
+// Determine if we're in development or production
+const isDev = __dirname.includes("src");
+const commandsPath = isDev
+  ? path.join(__dirname, "commands")
+  : path.join(__dirname, "commands");
+
+// Use appropriate file extension based on environment
+const fileExtension = isDev ? ".ts" : ".js";
+
+// Check if commands directory exists
+if (existsSync(commandsPath)) {
+  // Loop through all the commands folder with appropriate extension
+  readdirSync(commandsPath)
+    .filter((file) => file.endsWith(fileExtension))
+    .forEach((file) => {
+      // Get the command from the file
+      const command = require(path.join(commandsPath, file)).default;
+      // Add the command to the commands array
+      Commands.push(command);
+    });
+}
 
 export default Commands;
